5dc4f28bed42745db5fd10f0eabde5bc86c225a2
Nazmol3215 <mdasikahamad2006@gmail.com>
2024-09-02 15:08:14 +0600

    add a remove button spacing

--- Scripts.js
+++ Scripts.js
@@ -3,6 +3,24 @@ scrollltop.addEventListener("click", smoothScroll);
 function smoothScroll() {
     window.scrollTo(40, 50);
 };
+git
 
+
+
+function search() {
+    let filter = document.getElementById('find').value.toUpperCase();
+    let item = document.querySelectorAll('.containerFirst');
+    let l = document.getElementsByTagName('h4');
+    for (var i = 0; i <= l.length; i++) {
+        let a = item[i].getElementsByTagName('h4')[0];
+        let value = a.innerHTML || a.innerText || a.textContent;
+        if (value.toUpperCase().indexOf(filter) > -1) {
+            item[i].style.display = "";
+        }
+        else {
+            item[i].style.display = "none";
+        }
+    }
+}
 
 
